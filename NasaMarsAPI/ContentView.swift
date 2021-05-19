64fe7f64db7acf29c9debed969f00f2c5afe4efb
erennlk <elik.dark91@icloud.com>
2021-05-19 11:17:26 +0300

    Initial Commit

---
+++ NasaMarsAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NasaMarsAPI
+//
+//  Created by Eren  Çelik on 19.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NasaMarsAPI/NasaMarsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaMarsAPIApp.swift
+//  NasaMarsAPI
+//
+//  Created by Eren  Çelik on 19.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct NasaMarsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
